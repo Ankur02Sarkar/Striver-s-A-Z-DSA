e0d2f23210451ad13e58b3ce60fca0343568220f
Ankur Sarkar <78259188+Ankur02Sarkar@users.noreply.github.com>
2022-08-30 10:54:04 +0530

    Create 2-reverseANumber.cpp

---
+++ 1.3 Basic Maths/2-reverseANumber.cpp
@@ -0,0 +1,45 @@
+
+/*********************************************** Reverse Bits (32bits) GFG**********************************************************/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long X) {
+        /*
+        // Approach 1
+        string num = bitset<32>(X).to_string();
+        reverse(num.begin(), num.end());
+        return stoll(num, 0, 2);
+        */
+        long long res = 0;
+        for(int i = 0 ; i < 32 ; i++){
+            long long lsb = X & 1;
+            long long reverseLsb = lsb << (31-i);
+            res = res | reverseLsb;
+            X = X >> 1;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+/****************************************************************************************************************************************************************/
